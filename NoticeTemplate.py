00dc609e7fb01ab2a703a29e82d2be1db4d673c1
Alan <a33845637@gmail.com>
2024-10-31 16:13:05 +0800

    Update NoticeTemplate.py

--- NoticeTemplate.py
+++ NoticeTemplate.py
@@ -1,13 +1,8 @@
 # 普通模板
 def normalTemplate():
-    content = """{content}---
-数据日期:{date}
-今日总收益:{total_today}
-总可用积分:{avail_today}
-绑定账户:{account}
-设备总数:{devicesCount}
-**设备信息如下:**
-{detail}"""
+    content = """
+🥳今日收益:{total_today}，总可用积分:{avail_today}
+"""
     return content
 
 # Html模板
